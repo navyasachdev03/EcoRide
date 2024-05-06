ab8c91c4d165f459e4bbdde2dfacc49a4e221f83
navyasachdev03 <105008906+navyasachdev03@users.noreply.github.com>
2024-05-07 00:59:07 +0530

    Update RideDetails.js

--- frontend/src/RideDetails.js
+++ frontend/src/RideDetails.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { FaPhone, FaCarRear } from "react-icons/fa6";
 import { GoDot } from "react-icons/go";
+import { useRides } from './RidesContext';
 
 function RideDetails({ ride }) {
 
     const imgPath = `livePhoto-${ride.driverContact}.jpeg`;
+    const { bookRide } = useRides();
+
+    function handleBookRide(){
+        
+        alert('Your ride has been booked successfully!')
+        bookRide(ride);
+
+    }
 
     return (
         <div>
@@ -34,7 +43,6 @@ function RideDetails({ ride }) {
                     </div>
                     <div className="md:flex justify-between">
                         <div className="flex md:py-4 mt-4">
-                            {/* <FaUserCircle className="mr-4 text-5xl" /> */}
                             <img src={imgPath} className="h-20 w-20 rounded-full" />
                             <div className="flex-col p-4">
                                 <p>{ride.driverName}</p>
@@ -52,7 +60,7 @@ function RideDetails({ ride }) {
                         </div>
                     </div>
                     <div className="md:py-4">
-                        <button className="bg-emerald-800 hover:bg-emerald-600 text-white font-bold py-2 px-8 rounded w-auto">Book</button>
+                        <button className="bg-emerald-800 hover:bg-emerald-600 text-white font-bold py-2 px-8 rounded w-auto" onClick={handleBookRide}>Book</button>
                     </div>
                 </div>
             </div>
